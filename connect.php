6f8b4a586fd4116a71fa6d89f84ef8890d1dc373
Omoseke <olaseke20@gmail.com>
2024-02-06 21:48:33 +0100

    This is a PHP script to test my database connection

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    $servername = "localhost";
+    $username = "sekisko";
+    $password = "a9vRNgkhL6J(qeY3";
+    $database = "sekisko";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password,$database);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+?>
